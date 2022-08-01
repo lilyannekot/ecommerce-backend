32d84f064c77280e3e7928e6d122cc2301b3cb0c
lilys tacos <lilyannek@gmail.com>
2022-07-31 22:46:01 -0700

    put route to update a category by its id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -59,11 +59,25 @@ router.post("/", (req, res) => {
 });
 
 router.put("/:id", (req, res) => {
-  // update a category by its `id` value
+  // Update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((categoryData) => {
+      if (categoryData) {
+        res.status(404).json({ message: "No category could be found!" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
+  // Delete a category by its `id` value
 });
 
 module.exports = router;
